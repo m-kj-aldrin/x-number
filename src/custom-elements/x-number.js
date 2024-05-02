ad578f65876d68d3a257caf2e6e2ee7ef3421869
m-kj-aldrin <mkj.aldrin@icloud.com>
2024-05-02 13:25:17 +0200

    correct parsing of text

--- src/custom-elements/x-number.js
+++ src/custom-elements/x-number.js
@@ -2,17 +2,29 @@ const customNumbertemplate = document.createElement("template");
 customNumbertemplate.innerHTML = `
 <style>
   :host{
-    display: block;
-    border: 1px currentColor solid;
+    max-width: max-content;
   }
   [contenteditable="true"]{
-    background-color: red;
+    /*border: 1px currentColor solid;*/
+    border-bottom: 1px currentColor solid;
+    min-width: 2ch;
+    width: max-content;
+  }
+  [contenteditable="true"]:focus{
+    outline: 1px currentColor solid;
+    outline-offset: 2px;
+  }
+  ::slotted([slot="label"]){
+    margin-bottom: 2px;
   }
 </style>
+<slot name="label"></slot>
 <div contenteditable="true"></div>
 `;
 
 export class CustomNumberElement extends HTMLElement {
+  #value = 0;
+
   constructor() {
     super();
 
@@ -23,12 +35,38 @@ export class CustomNumberElement extends HTMLElement {
     this.#attachListeners();
   }
 
+  get value() {
+    return this.#value;
+  }
+
+  set value(value) {
+    this.#value = value;
+  }
+
   #attachListeners() {
     this.shadowRoot.addEventListener("input", this.#handelEditInput.bind(this));
   }
 
   /**@param {HTMLInputEvent} e */
   #handelEditInput(e) {
-    if (e.target.contentEditable == "true") e.stopPropagation();
+    if (e.target.contentEditable == "true") {
+      e.stopPropagation();
+      let matchArray = this.#parseValue(e.target.textContent);
+
+      if (matchArray.length) {
+        this.value = parseFloat(matchArray[0]);
+        this.dispatchEvent(new InputEvent("input", { bubbles: true }));
+      }
+    }
+  }
+
+  /**@param {string} inputString */
+  #parseValue(inputString) {
+    // Regex to exclude integers followed directly by a decimal without digits after, and match proper floating point numbers
+    const regex = /\b\d+\b(?!\.\d*)(?!\.\b)|\b\d+\.\d+\b/g;
+    let matches = inputString.match(regex);
+
+    // Return the matches or an empty array if no matches found
+    return matches || [];
   }
 }
--- test/app.js
+++ test/app.js
@@ -1,9 +1,15 @@
 import "../src/custom-elements/index.js";
+import { CustomNumberElement } from "../src/custom-elements/x-number.js";
 
 const number0 = document.createElement("x-number");
+number0.innerHTML = `
+<div slot="label">number</div>
+`;
 
 document.body.append(number0);
 
 document.body.addEventListener("input", (e) => {
-  console.log("body input", e);
+  if (e.target instanceof CustomNumberElement) {
+    console.log("number: ", e.target.value);
+  }
 });
